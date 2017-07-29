bcc22d4b6d3c4ce3693ba19ffeeddd08531e5414
Martin Eden <martin.s.eden@gmail.com>
2017-07-29 14:20:15 +0100

    Working "Hello world" app without tests.

---
+++ src/main/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+require('file-loader?name=[name].[ext]!./index.html');
+
+class App extends React.Component<undefined, undefined> {
+	render() {
+		return <div>Hello world from React, Webpack and TypeScript!</div>;
+	}
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("react")
+);
